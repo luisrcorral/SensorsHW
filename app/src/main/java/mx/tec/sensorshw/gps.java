220b9bf0c408853c12e782a8af7bef2ca2b2a120
luisrcorral <luisrcorral@gmail.com>
2022-03-21 15:43:20 -0600

    Adding GPS activity

---
+++ app/src/main/java/mx/tec/sensorshw/gps.java
@@ -0,0 +1,53 @@
+package mx.tec.sensorshw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class gps extends AppCompatActivity implements LocationListener {
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
+    TextView txtLat;
+    String lat;
+    String provider;
+    protected String latitude,longitude;
+    protected boolean gps_enabled,network_enabled;
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtLat = findViewById(R.id.textViewLoc);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        txtLat = findViewById(R.id.textViewLoc);
+        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+       //
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        //
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        //
+    }
+}
