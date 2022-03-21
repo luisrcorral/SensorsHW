28153dea226adbdf59f3ccda859175323e83fc42
luisrcorral <luisrcorral@gmail.com>
2022-03-21 15:44:39 -0600

    Minor updates

--- app/src/main/java/mx/tec/sensorshw/MainActivity.java
+++ app/src/main/java/mx/tec/sensorshw/MainActivity.java
@@ -93,7 +93,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button gpsButton= findViewById(R.id.buttonGPS);
+        gpsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent i = new Intent(context, gps.class);
+                startActivity(i);
+            }
+        });
 
 
     }
